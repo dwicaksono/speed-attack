b51fea2cdfa0e56d7a63bc97d0fe4fbaf6a7bf6b
odddyyy <56987696+odddyyy@users.noreply.github.com>
2020-03-19 14:25:17 +0700

    Intializing socket.io in server and client

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <form action="">
+      <ul id="messages"></ul>
+      <input id="m" autocomplete="off" /><button>Send</button>
+    </form>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+const socket = io('http://localhost:3000')
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  methods: {
   }
 }
 </script>
